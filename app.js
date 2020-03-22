13fb26760d9cd17f44534b24d5ab570ce95307a4
fluffy <fluffy@fluffys-MacBook-Pro.local>
2020-03-21 21:57:31 -0500

    adding & removing note hooked

---
+++ app.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const yargs = require("yargs");
+const notes = require("./notes");
+
+// Add yargs app version
+yargs.version("1.0.0");
+
+// Create add note COMMAND
+yargs.command({
+  command: "add",
+  describe: "Add new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demmandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note body content",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+// Create remove note COMMAND
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demmandOption: true,
+      type: "string"
+    }
+  },
+  handler: argv => {
+    notes.removeNote(argv.title);
+  }
+});
+
+// Create add command
+yargs.parse();
---
+++ notes.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+// Add note function
+const addNote = (title, body) => {
+  const notes = loadNotes();
+
+  const duplicatedNotes = notes.filter(note => {
+    return note.title === title;
+  });
+
+  // if there is no matching title
+  if (duplicatedNotes.length === 0) {
+    notes.push({
+      title: title,
+      body: body
+    });
+
+    // data to file
+    saveNote(notes);
+    console.log(chalk.green.inverse("Note is successfully added!"));
+  } else {
+    console.log(chalk.red.inverse(`Title "${title}" already exists!`));
+  }
+};
+
+// Remove note function
+const removeNote = title => {
+  const notes = loadNotes();
+
+  const notesToKeep = notes.filter(note => {
+    return note.title !== title;
+  });
+
+  // If any match found then udpate data
+  if (notes.length > notesToKeep.length) {
+    saveNote(notesToKeep);
+    console.log(chalk.green.inverse("Note successfully removed!"));
+  } else {
+    console.log(chalk.red.inverse(`Note with "${title}" not found!`));
+  }
+};
+
+// Save notes
+const saveNote = note => {
+  const dataJSON = JSON.stringify(note);
+  fs.writeFileSync("notes.json", dataJSON);
+};
+
+// Load notes from file
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  addNote: addNote,
+  removeNote: removeNote
+};
